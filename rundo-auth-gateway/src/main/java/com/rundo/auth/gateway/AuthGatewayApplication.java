c74b284d9472731227be2b19e32da01a796bdf4f
jiangyu <105194@runjian.com>
2022-12-26 21:18:46 +0800

    网关服务基本服务能力

---
+++ rundo-auth-gateway/src/main/java/com/rundo/auth/gateway/AuthGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.rundo.auth.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jiang4Yu
+ * @version V1.0.0
+ * @ClassName AuthGatewayApplication
+ * @Description 应用启动类
+ * @date 2022-12-22 周四 14:41
+ */
+@SpringBootApplication
+public class AuthGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthGatewayApplication.class, args);
+    }
+}
