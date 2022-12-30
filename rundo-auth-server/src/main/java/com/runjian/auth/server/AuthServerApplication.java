f799a44028a05f58eff81ded7d489eaaa0a90457
jiangyu <105194@runjian.com>
2022-12-30 11:13:35 +0800

    暂存

--- rundo-auth-server/src/main/java/com/runjian/auth/server/AuthServerApplication.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/AuthServerApplication.java
@@ -1,8 +1,10 @@
 package com.runjian.auth.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Jiang4Yu
@@ -14,8 +16,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 // 开启服务注册功能配置功能
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.runjian.auth.server.service")
+@MapperScan(basePackages = "com.runjian.auth.server.mapper")
 public class AuthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+
     }
+
 }
