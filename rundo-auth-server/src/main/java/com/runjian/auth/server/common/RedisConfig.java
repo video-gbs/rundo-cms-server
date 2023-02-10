ffaed3799cb71fb28a21cd4d78269b660bed7769
jiangyu <105194@runjian.com>
2023-02-10 14:23:51 +0800

    剔除无用代码

--- rundo-auth-server/src/main/java/com/runjian/auth/server/common/RedisConfig.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/common/RedisConfig.java
@@ -1,6 +1,5 @@
-package com.runjian.auth.server.config;
+package com.runjian.auth.server.common;
 
-import com.runjian.auth.server.common.FastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
