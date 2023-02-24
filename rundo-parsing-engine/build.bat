84122eed41de7a2469bbbbc1c1844910c06e3cab
Miracle <k22227011@outlook.com>
2023-02-24 15:32:27 +0800

    优化编译脚本

--- rundo-parsing-engine/build.bat
+++ rundo-parsing-engine/build.bat
@@ -3,8 +3,8 @@ if not exist .env (
   echo .env file not exist
   goto end
 )
-
-for /f "tokens=1,2 delims==" %%i in (.env) do (
-  docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/hyy-rundo-gms/rundo-parsing-engine:%%j .
-  docker push registry.cn-hangzhou.aliyuncs.com/hyy-rundo-gms/rundo-parsing-engine:%%j
-)
+set version=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%
+echo version=%version% > .env
+docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/hyy-rundo-gms/rundo-device-control:%version% .
+docker push registry.cn-hangzhou.aliyuncs.com/hyy-rundo-gms/rundo-device-control:%version%
+echo Build Successfully version=%version%
