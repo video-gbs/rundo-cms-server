35267ec9a2a1d3b5d09c09ef0026ee2befd1a84c
Miracle <k22227011@outlook.com>
2023-03-13 15:48:45 +0800

    增加Feign授权配置

---
+++ rundo-device-control/src/main/java/com/runjian/device/config/FeignConfig.java
@@ -0,0 +1,34 @@
+package com.runjian.device.config;
+
+import com.runjian.common.constant.CommonConstant;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * Feign配置
+ * 使用FeignClient进行服务间调用，传递headers信息
+ * @author chenjialing
+ */
+@Configuration
+public class FeignConfig implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Enumeration<String> headersNames = request.getHeaderNames();
+        while (headersNames.hasMoreElements()){
+            String name = headersNames.nextElement();
+            if(CommonConstant.AUTHORIZATION.equalsIgnoreCase(name)){
+                //添加token
+                requestTemplate.header(CommonConstant.AUTHORIZATION, request.getHeader(name));
+            }
+        }
+
+    }
+}
