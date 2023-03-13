c0edd61d05f1050d44c26ca8f9624b8129228f79
jiangyu <105194@runjian.com>
2023-03-13 09:28:34 +0800

    梳理关联用户表

--- rundo-auth-server/src/main/java/com/runjian/auth/server/controller/system/RoleInfoController.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/controller/system/RoleInfoController.java
@@ -17,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -108,7 +109,7 @@ public class RoleInfoController {
 
     @PostMapping("/relationUserByRole")
     @ApiOperation("查询已关联用户列表")
-    public CommonResponse<IPage<RelationSysUserInfoVO>> listRelationUser(@RequestBody QueryRoleRelationSysUserInfoDTO dto) {
+    public CommonResponse<IPage<RelationSysUserInfoVO>> listRelationUser(@RequestBody @Valid QueryRoleRelationSysUserInfoDTO dto) {
         log.info("查询已关联用户列表传参{}", JSONUtil.toJsonStr(dto));
         return CommonResponse.success(roleInfoService.listRelationUser(dto));
     }
