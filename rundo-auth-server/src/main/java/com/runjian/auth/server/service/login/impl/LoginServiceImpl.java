f33678701a16750e6c2764d6daaef89ef5c32124
jiangyu <105194@runjian.com>
2023-03-15 18:06:24 +0800

    修复：【组织管理-用户管理】账号已经过了有效结束时间，账号应不可登录

--- rundo-auth-server/src/main/java/com/runjian/auth/server/service/login/impl/LoginServiceImpl.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/service/login/impl/LoginServiceImpl.java
@@ -3,6 +3,7 @@ package com.runjian.auth.server.service.login.impl;
 import cn.dev33.satoken.secure.BCrypt;
 import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.date.LocalDateTimeUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.pig4cloud.captcha.SpecCaptcha;
 import com.pig4cloud.captcha.base.Captcha;
@@ -54,6 +55,10 @@ public class LoginServiceImpl implements LoginService {
         if (!BCrypt.checkpw(password, userInfo.getPassword())) {
             throw new BusinessException(BusinessErrorEnums.VALID_BIND_EXCEPTION_ERROR, "密码错误");
         }
+        // 检测账户是否已过期
+        if (isAccountNonExpired(userInfo)) {
+            throw new BusinessException(BusinessErrorEnums.USER_ACCOUNT_NOT_ENABLED, "账户已过期");
+        }
         // 登录
         StpUtil.login(userInfo.getId());
         // 获取token
@@ -77,7 +82,7 @@ public class LoginServiceImpl implements LoginService {
         SpecCaptcha captcha = new SpecCaptcha(130, 48, 5);
         captcha.setCharType(Captcha.TYPE_ONLY_NUMBER);
         String verCode = captcha.text().toLowerCase();
-        String key = "captcha:" + UUID.randomUUID().toString().replace("-", "") ;
+        String key = "captcha:" + UUID.randomUUID().toString().replace("-", "");
         redisCache.setCacheObject(key, verCode, 30, TimeUnit.MINUTES);
         Map<String, String> map = new HashMap<>();
         map.put("key", key);
@@ -90,5 +95,12 @@ public class LoginServiceImpl implements LoginService {
         StpUtil.logout();
     }
 
+    private boolean isAccountNonExpired(UserInfo userInfo) {
+        if (null != userInfo.getExpiryDateEnd()) {
+            long flag = LocalDateTimeUtil.between(LocalDateTimeUtil.now(), userInfo.getExpiryDateEnd()).toDays();
+            return flag > 0;
+        }
+        return true;
+    }
 }
 
