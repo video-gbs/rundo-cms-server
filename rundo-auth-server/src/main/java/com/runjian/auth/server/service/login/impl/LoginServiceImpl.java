99e50f914e45796ecabad2e13e472f7c195b9344
jiangyu <105194@runjian.com>
2023-03-15 21:28:37 +0800

    修复：【组织管理-用户管理】账号已经过了有效结束时间，账号应不可登录 修复：【组织管理-用户管理】账号已禁用，账号应不可登录

--- rundo-auth-server/src/main/java/com/runjian/auth/server/service/login/impl/LoginServiceImpl.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/service/login/impl/LoginServiceImpl.java
@@ -56,8 +56,12 @@ public class LoginServiceImpl implements LoginService {
             throw new BusinessException(BusinessErrorEnums.VALID_BIND_EXCEPTION_ERROR, "密码错误");
         }
         // 检测账户是否已过期
-        if (isAccountNonExpired(userInfo)) {
+        if (!isAccountNonExpired(userInfo)) {
             throw new BusinessException(BusinessErrorEnums.USER_ACCOUNT_NOT_ENABLED, "账户已过期");
+        }
+        // 检测账户是否已禁用
+        if (isEnabled(userInfo)){
+            throw new BusinessException(BusinessErrorEnums.USER_ACCOUNT_NOT_ENABLED, "账户已禁用");
         }
         // 登录
         StpUtil.login(userInfo.getId());
@@ -95,12 +99,42 @@ public class LoginServiceImpl implements LoginService {
         StpUtil.logout();
     }
 
+    /**
+     * 当前账号是否未过期
+     *
+     * @param userInfo 用户信息
+     * @return true 没过期 false 已过期
+     */
     private boolean isAccountNonExpired(UserInfo userInfo) {
         if (null != userInfo.getExpiryDateEnd()) {
             long flag = LocalDateTimeUtil.between(LocalDateTimeUtil.now(), userInfo.getExpiryDateEnd()).toDays();
             return flag > 0;
         }
         return true;
+    }
+
+    /**
+     * 当前账号是否未锁定
+     *
+     * @param userInfo
+     * @return
+     */
+    private boolean isAccountNonLocked(UserInfo userInfo) {
+        // 禁用状态;0:启用(否）,1:禁用(是)
+        Integer locked = 1;
+        return locked.equals(userInfo.getStatus());
+    }
+
+    /**
+     * 当前账号是否被允许
+     *
+     * @param userInfo
+     * @return
+     */
+    private boolean isEnabled(UserInfo userInfo) {
+        // 禁用状态;0:启用(否）,1:禁用(是)
+        Integer locked = 1;
+        return locked.equals(userInfo.getStatus());
     }
 }
 
