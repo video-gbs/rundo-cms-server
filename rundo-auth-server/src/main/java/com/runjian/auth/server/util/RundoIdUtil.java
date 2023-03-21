441bf9725fe2e8ef820f66b4edb8c03e7313b7a0
Miracle <k22227011@outlook.com>
2023-03-21 10:43:40 +0800

    修改device-control配置文件

--- rundo-auth-server/src/main/java/com/runjian/auth/server/util/RundoIdUtil.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/util/RundoIdUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RundoIdUtil {
     Snowflake snowflake = IdUtil.getSnowflake(1, 1);
+
     public Long nextId() {
         return snowflake.nextId();
     }
