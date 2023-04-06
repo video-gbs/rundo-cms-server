92bf75b6cc619566ba2ab6110b86f26b7415ec61
chenjialing <595168663@qq.com>
2023-04-06 15:40:23 +0800

    开发----预置位相关接口入参出参定义

---
+++ rundo-expansion-server/src/main/java/com/runjian/device/expansion/mapper/ChannelPresetMapper.java
@@ -0,0 +1,12 @@
+package com.runjian.device.expansion.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.runjian.device.expansion.entity.ChannelPresetLists;
+
+/**
+ * @author chenjialing
+ */
+public interface ChannelPresetMapper extends BaseMapper<ChannelPresetLists> {
+
+
+}
