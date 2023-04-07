7909f16be08748f90df18e81d595a52a67b005a9
jiangyu <105194@runjian.com>
2023-04-07 14:46:44 +0800

    去除druid底部的广告配置类,注释补齐

--- rundo-auth-server/src/main/java/com/runjian/auth/server/config/RemoveDruidAdConfig.java
+++ rundo-auth-server/src/main/java/com/runjian/auth/server/config/RemoveDruidAdConfig.java
@@ -17,16 +17,23 @@ import java.io.IOException;
  * @author Jiang4Yu
  * @version V1.0.0
  * @ClassName DruidConfig
- * @Description druid 配置
+ * @Description 去除druid底部的广告配置类
  * @date 2023-03-07 周二 15:44
  */
 @Configuration
-public class DruidConfig {
+public class RemoveDruidAdConfig {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource druid() {
         return new DruidDataSource();
     }
+
+    /**
+     * 除去页面底部的广告
+     *
+     * @param properties
+     * @return org.springframework.boot.web.servlet.FilterRegistrationBean
+     */
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Bean
