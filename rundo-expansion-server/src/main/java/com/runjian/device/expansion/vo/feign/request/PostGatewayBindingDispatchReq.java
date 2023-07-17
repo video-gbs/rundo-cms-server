2bcbe9f05ffd7c25c05a3e648c5957c44ff9a08f
chenjialing <595168663@qq.com>
2023-07-17 11:45:11 +0800

    扩展服务--服务管理接口开发

---
+++ rundo-expansion-server/src/main/java/com/runjian/device/expansion/vo/feign/request/PostGatewayBindingDispatchReq.java
@@ -0,0 +1,28 @@
+package com.runjian.device.expansion.vo.feign.request;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * 网关绑定流媒体服务请求体
+ * @author Miracle
+ * @date 2023/2/9 11:21
+ */
+@Data
+public class PostGatewayBindingDispatchReq {
+
+    /**
+     * 网关id
+     */
+    @NotNull(message = "网关id不能为空")
+    @Range(min = 1, message = "非法网关id")
+    private Long gatewayId;
+
+    /**
+     * 调度服务id
+     */
+    @Range(min = 1, message = "非法调度服务id")
+    private Long dispatchId;
+}
