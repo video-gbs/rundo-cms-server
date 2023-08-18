11079d34041fc6b05420f0e4a9daba3dc7ea317b
Miracle <k22227011@outlook.com>
2023-08-18 11:35:06 +0800

    修改druid配置

---
+++ rundo-device-control/src/main/java/com/runjian/device/config/DruidFilterConfig.java
@@ -0,0 +1,29 @@
+package com.runjian.device.config;
+
+
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * druid的拦截器配置
+ * @author Miracle
+ * @date 2023/8/7 17:32
+ */
+@Configuration
+@AllArgsConstructor
+public class DruidFilterConfig {
+
+    @Bean
+    public WallFilter wallFilter() {
+        WallConfig config = new WallConfig();
+        config.setMultiStatementAllow(true);
+        config.setNoneBaseStatementAllow(true);
+        WallFilter wallFilter = new WallFilter();
+        wallFilter.setConfig(config);
+        return wallFilter;
+    }
+}
