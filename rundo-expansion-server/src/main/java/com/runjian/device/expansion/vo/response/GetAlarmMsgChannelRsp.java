e61b35bffc7fa750cbb6460f81e15b573e1ba177
Miracle <k22227011@outlook.com>
2023-10-20 17:07:48 +0800

    修改告警编辑

--- rundo-expansion-server/src/main/java/com/runjian/device/expansion/vo/response/GetAlarmMsgChannelRsp.java
+++ rundo-expansion-server/src/main/java/com/runjian/device/expansion/vo/response/GetAlarmMsgChannelRsp.java
@@ -33,6 +33,11 @@ public class GetAlarmMsgChannelRsp {
     private String deviceName;
 
     /**
+     * 告警编码
+     */
+    private String alarmCode;
+
+    /**
      * 告警等级
      */
     private String alarmLevel;
