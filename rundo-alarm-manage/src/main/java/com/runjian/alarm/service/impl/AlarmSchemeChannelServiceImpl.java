ff5ba380643f6c0e36b3c710e2b2b60ba1b5e41a
Miracle <k22227011@outlook.com>
2023-11-10 17:00:18 +0800

    修改告警预案修改通道失败

--- rundo-alarm-manage/src/main/java/com/runjian/alarm/service/impl/AlarmSchemeChannelServiceImpl.java
+++ rundo-alarm-manage/src/main/java/com/runjian/alarm/service/impl/AlarmSchemeChannelServiceImpl.java
@@ -43,6 +43,8 @@ public class AlarmSchemeChannelServiceImpl implements AlarmSchemeChannelService 
 
     private final StringRedisTemplate redisTemplate;
 
+    private final RedissonClient redissonClient;
+
     private Map<String, PostMessageSubRsp> messageSubRspMap = new HashMap<>();
 
     private final AlarmSchemeChannelRelMapper alarmSchemeChannelRelMapper;
@@ -68,29 +70,29 @@ public class AlarmSchemeChannelServiceImpl implements AlarmSchemeChannelService 
             init();
             return;
         }
-        LocalDateTime nowTime = LocalDateTime.now();
-            if (redisLockUtil.lock(channelAddOrDelete.getMsgLock(), nowTime.toString(), 5, TimeUnit.SECONDS, 1)){
-                try{
-                    Map<Object, Object> entries = redisTemplate.opsForHash().entries(channelAddOrDelete.getMsgHandle());
-                    if (entries.isEmpty()){
-                        return;
-                    }
-                    Set<Long> deleteChannelIds = new HashSet<>();
-                    for (Map.Entry<Object, Object> entry : entries.entrySet()){
-                        JSONObject jsonObject = JSONObject.parseObject(entry.getValue().toString());
-                        Integer signState = jsonObject.getInteger("signState");
-                        if (Objects.equals(signState, SignState.DELETED.getCode())){
-                            deleteChannelIds.add(Long.parseLong(entry.getKey().toString()));
-                        }
-                    }
-                    List<AlarmSchemeChannelRel> alarmSchemeChannelRelList = alarmSchemeChannelRelMapper.selectByChannelIds(deleteChannelIds);
-                    if (!alarmSchemeChannelRelList.isEmpty()){
-                        alarmSchemeChannelRelMapper.batchDelete(alarmSchemeChannelRelList.stream().map(AlarmSchemeChannelRel::getId).collect(Collectors.toList()));
-                        alarmSchemeService.defense(alarmSchemeChannelRelList.stream().map(AlarmSchemeChannelRel::getChannelId).collect(Collectors.toList()), false);
-                    }
-                } finally {
-                    redisLockUtil.unLock(channelAddOrDelete.getMsgLock());
+        RLock rLock = redissonClient.getLock(channelAddOrDelete.getMsgLock());
+        if (rLock.tryLock()){
+            try{
+                Map<Object, Object> entries = redisTemplate.opsForHash().entries(channelAddOrDelete.getMsgHandle());
+                if (entries.isEmpty()){
+                    return;
                 }
+                Set<Long> deleteChannelIds = new HashSet<>();
+                for (Map.Entry<Object, Object> entry : entries.entrySet()){
+                    JSONObject jsonObject = JSONObject.parseObject(entry.getValue().toString());
+                    Integer signState = jsonObject.getInteger("signState");
+                    if (Objects.equals(signState, SignState.DELETED.getCode())){
+                        deleteChannelIds.add(Long.parseLong(entry.getKey().toString()));
+                    }
+                }
+                List<AlarmSchemeChannelRel> alarmSchemeChannelRelList = alarmSchemeChannelRelMapper.selectByChannelIds(deleteChannelIds);
+                if (!alarmSchemeChannelRelList.isEmpty()){
+                    alarmSchemeChannelRelMapper.batchDelete(alarmSchemeChannelRelList.stream().map(AlarmSchemeChannelRel::getId).collect(Collectors.toList()));
+                    alarmSchemeService.defense(alarmSchemeChannelRelList.stream().map(AlarmSchemeChannelRel::getChannelId).collect(Collectors.toList()), false);
+                }
+            } finally {
+                rLock.unlock();
             }
+        }
     }
 }
