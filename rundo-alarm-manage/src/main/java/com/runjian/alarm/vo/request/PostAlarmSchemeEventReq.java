81c6d7f7a9a9d08bef441eb59847d7541e7c912e
Miracle <k22227011@outlook.com>
2023-11-16 15:47:49 +0800

    告警初始化处理失败

--- rundo-alarm-manage/src/main/java/com/runjian/alarm/vo/request/PostAlarmSchemeEventReq.java
+++ rundo-alarm-manage/src/main/java/com/runjian/alarm/vo/request/PostAlarmSchemeEventReq.java
@@ -52,7 +52,7 @@ public class PostAlarmSchemeEventReq implements ValidatorFunction {
     /**
      * 录像时间 单位：秒 15、30、60
      */
-    @Range(min = 15, max = 60, message = "录像时间不正确")
+    @Range(min = 0, max = 60, message = "录像时间不正确")
     private Integer videoLength;
 
     /**
