4020fdaf3c804903147d502d6af5dc23e9fc869e
Miracle <k22227011@outlook.com>
2023-11-17 15:14:07 +0800

    告警初始化处理失败

--- rundo-alarm-manage/src/main/java/com/runjian/alarm/service/impl/AlarmEventHandleServiceImpl.java
+++ rundo-alarm-manage/src/main/java/com/runjian/alarm/service/impl/AlarmEventHandleServiceImpl.java
@@ -244,7 +244,9 @@ public class AlarmEventHandleServiceImpl implements AlarmEventHandleService {
     private PostRecordDownloadReq getPostRecordDownloadReq(AlarmMsgInfo alarmMsgInfo) {
         PostRecordDownloadReq postRecordDownloadReq = new PostRecordDownloadReq();
         postRecordDownloadReq.setChannelId(alarmMsgInfo.getChannelId());
-        postRecordDownloadReq.setEnableAudio(CommonEnum.getBoolean(alarmMsgInfo.getVideoAudioState()));
+        if (Objects.nonNull(alarmMsgInfo.getVideoAudioState())){
+            postRecordDownloadReq.setEnableAudio(CommonEnum.getBoolean(alarmMsgInfo.getVideoAudioState()));
+        }
         postRecordDownloadReq.setStreamType(2);
         postRecordDownloadReq.setPlayType(PlayType.ALARM.getCode());
         postRecordDownloadReq.setStartTime(alarmMsgInfo.getAlarmStartTime().plusSeconds(-5));
