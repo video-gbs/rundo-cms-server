b23234930d663675476cf3190f8897fa207ae494
Miracle <k22227011@outlook.com>
2023-12-11 10:21:33 +0800

    增加网关请求丢失日志打印

--- rundo-cascade-manage/src/main/java/com/runjian/cascade/CascadeManageApplication.java
+++ rundo-cascade-manage/src/main/java/com/runjian/cascade/CascadeManageApplication.java
@@ -1,8 +1,26 @@
 package com.runjian.cascade;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * @author Miracle
  * @date 2023/12/5 9:29
  */
+@RefreshScope
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
+@SpringBootApplication
+@ComponentScan(value = {"com.runjian.*"})
 public class CascadeManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CascadeManageApplication.class, args);
+    }
 }
