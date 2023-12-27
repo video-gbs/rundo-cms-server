c42443bcbae6390d9f058d0c42ed94c3f5bdb955
chenjialing <595168663@qq.com>
2023-12-27 09:04:49 +0800

    级联服务--注销流程开发

--- rundo-cascade-manage/src/main/java/com/runjian/cascade/gb28181Module/service/impl/PlatformCommandServiceImpl.java
+++ rundo-cascade-manage/src/main/java/com/runjian/cascade/gb28181Module/service/impl/PlatformCommandServiceImpl.java
@@ -2,10 +2,14 @@ package com.runjian.cascade.gb28181Module.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.runjian.cascade.conf.SipConfig;
+import com.runjian.cascade.gb28181Module.common.constant.SipBusinessConstants;
 import com.runjian.cascade.gb28181Module.gb28181.bean.OtherPlatform;
 import com.runjian.cascade.gb28181Module.gb28181.bean.ParentPlatform;
+import com.runjian.cascade.gb28181Module.gb28181.bean.SipTransactionInfo;
+import com.runjian.cascade.gb28181Module.gb28181.conf.DynamicTask;
 import com.runjian.cascade.gb28181Module.gb28181.transmit.cmd.ISIPCommanderForPlatform;
 import com.runjian.cascade.gb28181Module.service.IPlatformCommandService;
+import com.runjian.cascade.gb28181Module.service.IRedisCatchStorageService;
 import com.runjian.common.config.exception.BusinessErrorEnums;
 import com.runjian.common.config.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
@@ -26,22 +30,28 @@ public class PlatformCommandServiceImpl implements IPlatformCommandService {
     @Autowired
     SipConfig sipConfig;
 
+    @Autowired
+    private DynamicTask dynamicTask;
+
+    @Autowired
+    private IRedisCatchStorageService redisCatchStorageService;
 
     private ParentPlatform convertPlatformSip(OtherPlatform otherPlatform){
         ParentPlatform parentPlatform = new ParentPlatform();
         BeanUtil.copyProperties(otherPlatform,parentPlatform);
         parentPlatform.setExpires(sipConfig.getExpires());
         parentPlatform.setCharacterSet(sipConfig.getCharacterSet());
+        sipConfig.completePlatfrom(parentPlatform);
         return parentPlatform;
 
     }
 
     @Override
-    public Boolean register(OtherPlatform otherPlatform) throws InterruptedException {
+    public Boolean register(OtherPlatform otherPlatform) {
         ParentPlatform parentPlatform = convertPlatformSip(otherPlatform);
         AtomicReference<Boolean> flag = new AtomicReference<>(true);
         String serverGbId = parentPlatform.getServerGbId();
-        String lockKey = serverGbId+"_register";
+        String lockKey = serverGbId+SipBusinessConstants.PLATFORM_REGISTER_LOCK_PREFIX;
         locks.putIfAbsent(lockKey, new CountDownLatch(1));
         try {
 
@@ -56,29 +66,47 @@ public class PlatformCommandServiceImpl implements IPlatformCommandService {
         }catch (Exception e){
             throw  new BusinessException(BusinessErrorEnums.CASCADE_REGISTER_ERROR);
         }
+        try {
+            locks.get(lockKey).await(10,TimeUnit.SECONDS);
 
-        locks.get(lockKey).await(10,TimeUnit.SECONDS);
-        Boolean b = flag.get();
-        if(b){
-            //成功 设置缓存 启动定时注册周期  以及进行定时心跳发送
-            
-        }else {
-            //异步发送时的失败，或则异常的流程 不处理
+        }catch (InterruptedException e){
+            e.printStackTrace();
+
         }
-
-        return b;
+        return flag.get();
     }
 
 
+
     @Override
-    public void unRegister(OtherPlatform otherPlatform) {
+    public Boolean unRegister(OtherPlatform otherPlatform) {
         ParentPlatform parentPlatform = convertPlatformSip(otherPlatform);
+        AtomicReference<Boolean> flag = new AtomicReference<>(true);
 
+        String lockKey = parentPlatform.getServerGbId()+SipBusinessConstants.PLATFORM_UNREGISTER_LOCK_PREFIX;
+        locks.putIfAbsent(lockKey, new CountDownLatch(1));
         try {
-
+            SipTransactionInfo sipTransactionInfo = redisCatchStorageService.queryPlatformRegisterSip(parentPlatform.getServerGbId());
+            isipCommanderForPlatform.unregister(parentPlatform, sipTransactionInfo, (eventResult)->{
+                flag.set(false);
+                CountDownLatch latch = locks.remove(lockKey);
+                if (latch != null) {
+                    latch.countDown();
+                }
+            }, eventResult -> {
+                log.info("[国标级联] 注销成功， 平台：{}", parentPlatform.getServerGbId());
+            });
 
         }catch (Exception e){
             throw  new BusinessException(BusinessErrorEnums.CASCADE_UNREGISTER_ERROR);
         }
+        try {
+            locks.get(lockKey).await(10,TimeUnit.SECONDS);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+
+        }
+        return flag.get();
     }
 }
